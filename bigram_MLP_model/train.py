5514eaf16e5a673dcc56eaefdffdcf80716700ea
evfus <paslarumatei2006@gmail.com>
2026-05-25 20:09:21 +0300

    Add split evaluation function

--- bigram_MLP_model/train.py
+++ bigram_MLP_model/train.py
@@ -5,12 +5,14 @@ from utils import build_vocab, build_splits
 def train(model, path, iterations):
     words, stoi, itos = build_vocab(path)
 
-    Xtr, Ytr, _, _, _, _ = build_splits(words, stoi, model.context_size)
-    
+    Xtr, Ytr, Xdev, Ydev, Xtest, Ytest = build_splits(words, stoi, model.context_size)
+
     model.train()
 
     if iterations == "":
         iterations = 10000
+    else:
+        iterations = int(iterations)
 
     for step in range(iterations):
         ix = torch.randint(0, Xtr.shape[0], (32,))
@@ -25,9 +27,29 @@ def train(model, path, iterations):
 
         loss.backward()
 
-        lr = 1 if step < 100000 else 0.05
+        lr = 0.01 if step < 2000 else 0.005
         for p in model.parameters:
             p.data += -lr * p.grad
 
-        if step % 200 == 0:
+        if step % 1000 == 0:
             print(f'{step} /{iterations}: {loss.item()}')
+    
+    response = input("Do you want to compare loss between splits? ([y]/n) ")
+    if response == 'y' or response == '':
+        train_loss = evaluate_split(model, Xtr, Ytr)
+        dev_loss = evaluate_split(model, Xdev, Ydev)
+        test_loss = evaluate_split(model, Xtest, Ytest)
+        
+        print(f'\nTraining loss: {train_loss}')
+        print(f'Dev loss: {dev_loss}')
+        print(f'Test loss: {test_loss}\n')
+
+
+@torch.no_grad()
+def evaluate_split(model, X, Y):
+    model.eval()
+
+    logits = model.forward(X)
+    loss = model.loss(logits, Y)
+
+    return loss
