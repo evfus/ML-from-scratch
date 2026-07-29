2f9fa44adf0a91706130f0c77b108bd93fd2a1be
evfus <paslarumatei2006@gmail.com>
2026-07-29 23:09:15 +0300

    finish generate function

--- decoder_transformer/generate.py
+++ decoder_transformer/generate.py
@@ -1,13 +1,20 @@
 import config
 import dataset
-import model
+from model import DecoderTransformer
 import torch
 
 def generate():
-    #model = torch.load(...)
+    model = DecoderTransformer().to(config.device)
+    
+    checkpoint = torch.load(
+            "outputs/checkpoints/last.pt",
+            map_location = config.device
+    )
+
+    model.load_state_dict(checkpoint["model"])
 
     context = torch.zeros((1, 1), dtype = torch.long, device = config.device)
     output = model.generate(context, 2000)[0].tolist()
-    output = decode(output)
+    output = dataset.decode(output)
 
     return output
