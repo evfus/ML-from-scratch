1305261c0c551acbb13da112991ae2b13f0fa264
evfus <paslarumatei2006@gmail.com>
2026-07-29 23:19:45 +0300

    change generated output to save in text file

--- decoder_transformer/main.py
+++ decoder_transformer/main.py
@@ -5,11 +5,15 @@ from generate import generate
 parser = argparse.ArgumentParser()
 parser.add_argument("--mode", choices = ["train", "generate"])
 parser.add_argument("--resume", action = "store_true")
+
 args = parser.parse_args()
 
 if args.mode == "train":
     train(args.resume)
+
 elif args.mode == "generate":
-    print(generate())
+    out_file = open("outputs/output.txt", "w")
+    out_file.write(generate())
+
 else:
     print("error: the following arguments are required: --mode (use 'train' or 'generate')")
