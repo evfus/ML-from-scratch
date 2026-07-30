80ceaca687ea2d6a70d90f5bc695cf158cbf6951
evfus <80192555+evfus@users.noreply.github.com>
2026-07-30 16:48:53 +0300

    Update config.py

--- decoder_transformer/config.py
+++ decoder_transformer/config.py
@@ -1,15 +1,15 @@
 import torch
 
-batch_size = 32
-context_size = 8
-max_iters = 10000
-eval_interval = 50
-learning_rate = 1e-3
-eval_iters = 100
+batch_size = 64
+context_size = 256
+max_iters = 5000
+eval_interval = 100
+learning_rate = 3e-4
+eval_iters = 50
 
-n_embd = 32
-n_heads = 4
-n_layers = 4
+n_embd = 384
+n_heads = 6
+n_layers = 6
 dropout = 0.2
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
